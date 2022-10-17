cfeaf2a8bbbcce619efe0d1203fb2860850ee055
Samiksha Sivaji Bhor <89834694+SamikshaBhor20@users.noreply.github.com>
2022-10-17 13:20:46 +0530

    Add files via upload

---
+++ Data_Normalization.R
@@ -0,0 +1,19 @@
+#Reading an input cancer data file
+data_file=read.csv("E:/Samiksha/M.Sc_Bioinformatics/SEM_3/Cancer_Genomics/Cancer_genomics_practical/cancer.csv",sep=",",header=T,row.names = 1)
+
+#Create a count per matrix
+cpmatrix=data_file
+for(i in 1:ncol(data_file)){
+  cpmatrix[,i]=(data_file[,i]/sum(data_file[,i]))*1000000
+}
+
+#Calculate a log of cpm
+logcpm=log2(cpmatrix+1)
+
+mat=readRDS('log2cpm_matrix.rds')
+
+summary(logcpm)
+
+#Calculate a z score
+library(matrixStats)
+z_score = (logcpm - rowMeans(logcpm))/rowSds(as.matrix(logcpm))[row(logcpm)]
